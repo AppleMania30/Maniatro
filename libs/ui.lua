aab5df2ef87021ac0f5cd46df5b4650696a38154
AppleMania <sofialopezamaya379@gmail.com>
2026-03-03 00:08:57 +0100

    Add balatro asset to SMODS.Atlas configuration

--- libs/ui.lua
+++ libs/ui.lua
@@ -4,3 +4,12 @@ SMODS.Atlas({
 	px = 32,
 	py = 32
 })
+
+SMODS.Atlas({
+    key = "balatro", 
+    path = "balatro.png", 
+    px = 333,
+    py = 216,
+    prefix_config = { key = false },
+    atlas_table = "ASSET_ATLAS"
+})
